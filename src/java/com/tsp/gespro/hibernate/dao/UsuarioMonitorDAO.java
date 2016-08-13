a769b46dd6ec98eecd00543af6faf80ed20928fb
jfac <athiecampollo@gmail.com>
2016-08-13 18:16:34 -0500

    Template para envio de correo html

--- src/java/com/tsp/gespro/hibernate/dao/UsuarioMonitorDAO.java
+++ src/java/com/tsp/gespro/hibernate/dao/UsuarioMonitorDAO.java
@@ -11,6 +11,16 @@ import com.tsp.gespro.hibernate.pojo.UsuarioMonitor;
 import com.tsp.gespro.hibernate.pojo.HibernateUtil;
 import com.tsp.gespro.mail.TspMailBO;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.Map;
+import com.tsp.gespro.util.FileManage;
+
 
 /**
  *
@@ -132,10 +142,16 @@ public class UsuarioMonitorDAO {
         return lista(); 
     }    
      
-    private void enviarCorreoConCredenciales(UsuarioMonitor obj){
+    public void enviarCorreoConCredenciales(UsuarioMonitor obj){
         try {
             TspMailBO mail = new TspMailBO();
-            String contenido ="<b>Tus credenciales :</b><br/><br/>Usuario: <h3>"+ obj.getEmail() +"</h3><br/>Password: <h3>"+ obj.getPassword()+"</h3>";
+            //String contenido ="<b>Tus credenciales :</b><br/><br/>Usuario: <h3>"+ obj.getEmail() +"</h3><br/>Password: <h3>"+ obj.getPassword()+"</h3>";
+            Map<String,String> input = new HashMap<String,String>();
+            input.put("Usuario1",obj.getEmail());
+            input.put("Pass1",obj.getPassword());
+            String workingDir = System.getProperty("user.dir");
+            String contenido = readEmailFromHtml(workingDir+"/web/WEB-INF/templates/correo-GESPRO.HTML", input);
+            if(!contenido.isEmpty()){
             try {
                 String correoContacto = obj.getEmail();
                 mail.addTo(correoContacto, correoContacto);
@@ -144,9 +160,45 @@ public class UsuarioMonitorDAO {
             mail.addMessageMovilpyme(contenido,1);
             mail.setFrom(mail.getUSER(), mail.getFROM_NAME());            
             mail.send("Hola, tenemos tus credenciales GESPRO! " + obj.getEmail());
+            }
         } catch (Exception ex) {
             System.out.println("No se pudo enviar el correo de credenciales. Error: "+ ex.getMessage());
         }
+    }
+    //Method to replace the values for keys
+    protected String readEmailFromHtml(String filePath, Map<String, String> input) throws FileNotFoundException 
+    {
+        String msg = readContentFromFile(filePath);
+        try
+        {
+        Set<Entry<String, String>> entries = input.entrySet();
+        for(Map.Entry<String, String> entry : entries) {
+            msg = msg.replace(entry.getKey().trim(), entry.getValue().trim());
+        }
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Error al leer el template. Error: "+ ex.getMessage());
+        }
+        return msg;
+    }
+    private String readContentFromFile(String fileName) 
+    {
+        StringBuilder contents = new StringBuilder();
+
+        try {
+          try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line = null; 
+            while (( line = reader.readLine()) != null){
+              contents.append(line);
+              contents.append(System.getProperty("line.separator"));
+            }
+          }
+        }
+        catch (IOException ex){
+          System.out.println("Error al leer el template. Error: "+ ex.getMessage());
+        }
+        return contents.toString();
     }
     private void iniciaOperacion() throws HibernateException 
     { 
